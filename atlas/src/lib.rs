2555a0397f696c3f17c48fff490407ec6f813adb
Pete Gadomski <pete.gadomski@gmail.com>
2018-10-02 04:54:04 -0600

    Add full example for sbd messages

--- atlas/src/lib.rs
+++ atlas/src/lib.rs
@@ -1,11 +1,27 @@
-//! ATLAS status information.
+//! ATLAS is a collection of remote terrestrial LiDAR systems in southeast Greenland.
 //!
-//! ATLAS is a collection of remote terrestrial LiDAR systems. As of this writing, there are two
-//! ATLAS systems, one on the south and one on the north side of the Helheim Glacier in southeast
-//! Greenland.
+//! There are two ATLAS systems, ATLAS South and ATLAS North. Both ATLAS systems transmit
+//! regularly-scheduled messages called "heartbeats," which contain system status information. This
+//! crate parses those messages into serde-able data structures.
 //!
-//! ATLAS systems transmit regularly-scheduled messages called "heartbeats," which contain system
-//! status information.
+//! # Examples
+//!
+//! Heartbeat messages come in as one or more Iridium SBD messages. These messages are sent by
+//! Sutron data loggers, which append their own header information to the messages. The best way to
+//! create heartbeats from raw SBD messages is to use the `sutron` crate:
+//!
+//! ```
+//! # extern crate sutron;
+//! # extern crate atlas;
+//! # fn main() {
+//! use sutron::{Packet, Message};
+//! use atlas::Heartbeat;
+//! let packet_0 = Packet::from_path("fixtures/sbd/181002_050602.sbd").unwrap();
+//! let packet_1 = Packet::from_path("fixtures/sbd/181002_050622.sbd").unwrap();
+//! let message = Message::new(vec![packet_0, packet_1]).unwrap();
+//! let heartbeat = Heartbeat::new(&message).unwrap();
+//! # }
+//! ```
 
 #![deny(missing_docs, missing_debug_implementations, unsafe_code)]
 
